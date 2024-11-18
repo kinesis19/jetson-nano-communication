f49f45a481e9fde213152479f8aaf9c2f69c46a1
kinesis19 <lordkinesis@naver.com>
2024-11-18 21:48:42 +0900

    :package: Create: ROS2 with Qt package
    RO:BIT 지능 팀 19기, 최종 프로젝트를 위한 패키지 생성. 이 패키지의 주 목적과 용도는 최종 프로젝트 패키지를 위한 Laptop(Ubuntu), Jetson과, STM32의 통신 기능을 구현하는 것임. 이후에 intern organization에 모든 패키지(open cv, stm32, communication)를 통합할 예정임. 지금 패키지에서 Qt로 작업을 진행하는 이유는, 구현하는 김에 같이 UI를 배치하기 위함임 + 디버깅까지.

---
+++ final_project_internship/include/final_project_internship/qnode.hpp
@@ -0,0 +1,43 @@
+/**
+ * @file /include/final_project_internship/qnode.hpp
+ *
+ * @brief Communications central!
+ *
+ * @date February 2011
+ **/
+/*****************************************************************************
+** Ifdefs
+*****************************************************************************/
+
+#ifndef final_project_internship_QNODE_HPP_
+#define final_project_internship_QNODE_HPP_
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+#ifndef Q_MOC_RUN
+#include <rclcpp/rclcpp.hpp>
+#endif
+#include <QThread>
+
+/*****************************************************************************
+** Class
+*****************************************************************************/
+class QNode : public QThread
+{
+  Q_OBJECT
+public:
+  QNode();
+  ~QNode();
+
+protected:
+  void run();
+
+private:
+  std::shared_ptr<rclcpp::Node> node;
+
+Q_SIGNALS:
+  void rosShutDown();
+};
+
+#endif /* final_project_internship_QNODE_HPP_ */
